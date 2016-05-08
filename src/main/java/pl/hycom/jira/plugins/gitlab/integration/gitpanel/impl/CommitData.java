5d91b9a32d122390ae13435efb68f2ee19d7ee04
Tomasz Jędrzejewski <189675@edu.p.lodz.pl>
2016-05-08 17:14:31 +0200

    Refactoring pakietów

--- src/main/java/pl/hycom/jira/plugins/gitlab/integration/gitpanel/impl/CommitData.java
+++
@@ -1,8 +0,0 @@
-package pl.hycom.jira.plugins.gitlab.integration.gitpanel.impl;
-
-/**
- * Created by Thorgal on 08.04.2016.
- */
-public class CommitData {
-    //TO DO PIP-30 Implementacja pobierania informacji o commitach po stronie JIRY
-}
