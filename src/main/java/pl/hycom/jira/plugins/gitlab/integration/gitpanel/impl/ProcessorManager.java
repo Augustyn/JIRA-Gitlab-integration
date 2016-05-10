ec23d8f6ea7b78477020e4b2aa39d27a305c7148
Karol Joachimiak <kajoachimiak@gmail.com>
2016-05-10 10:08:21 +0200

    Dodano początkową wersję obsługi wyjątków

--- src/main/java/pl/hycom/jira/plugins/gitlab/integration/gitpanel/impl/ProcessorManager.java
+++ src/main/java/pl/hycom/jira/plugins/gitlab/integration/gitpanel/impl/ProcessorManager.java
@@ -2,6 +2,7 @@ package pl.hycom.jira.plugins.gitlab.integration.gitpanel.impl;
 
 
 import lombok.Setter;
+import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Service;
 import pl.hycom.jira.plugins.gitlab.integration.gitpanel.api.ProcessorInterface;
 
@@ -12,15 +13,21 @@ import java.util.List;
  * Created by Thorgal on 08.04.2016.
  */
 @Service
+@Log4j
 public class ProcessorManager {
     @Setter
     private List<Class<? extends ProcessorInterface>> processorsList = new ArrayList<>();
 
-    public void startProcessors(List<CommitData> commitInfoList) {
+    public void startProcessors(List<CommitData> commitInfoList){
         for (CommitData commitInfo : commitInfoList) {
             for (Class<? extends ProcessorInterface> processor : processorsList) {
                 ProcessorInterface interf = processor.cast(ProcessorInterface.class);
-                interf.execute(commitInfo);
+                try {
+                    interf.execute(commitInfo);
+                }
+                catch (Exception e){
+                    log.info("Processor throwed exception but it was ignored");
+                }
             }
         }
     }
