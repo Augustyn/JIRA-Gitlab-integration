baed5e66a186af6abff9264b566caa7270975eac
Kamil Rogowski <189760@edu.p.lodz.pl>
2016-05-19 15:08:49 +0200

    konfiguracja beanow przez xmle, dodane licencje, modyfikacja testu

--- src/main/java/pl/hycom/jira/plugins/gitlab/integration/dao/CommitRepository.java
+++ src/main/java/pl/hycom/jira/plugins/gitlab/integration/dao/CommitRepository.java
@@ -1,5 +1,20 @@
 package pl.hycom.jira.plugins.gitlab.integration.dao;
-
+/*
+ * <p>Copyright (c) 2016, Damian Deska, Kamil Rogowski
+ * Project:  gitlab-integration.</p>
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at</p>
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0</p>
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.</p>
+ */
 import lombok.extern.log4j.Log4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
@@ -24,7 +39,7 @@ public class CommitRepository implements ICommitDao {
     @Override
     public List<Commit> getNewCommits(int perPage, int pageNumber) {
         String urlMockWithPageNumber = urlMock + "?per_page=" + Integer.toString(perPage) + "&page=" + Integer.toString(pageNumber);
-        HttpEntity<?> requestEntity = new HttpEntity<Object>(new TemplateFactory().getHttpHeaders().setAuth(privateTokenMock).build());
+        HttpEntity<?> requestEntity = new HttpEntity<>(new TemplateFactory().getHttpHeaders().setAuth(privateTokenMock).build());
         ResponseEntity<List<Commit>> response = new TemplateFactory().getRestTemplate().exchange(urlMockWithPageNumber, HttpMethod.GET, requestEntity,
                 new ParameterizedTypeReference<List<Commit>>() {
                 });
@@ -35,7 +50,7 @@ public class CommitRepository implements ICommitDao {
     @Override
     public Commit getOneCommit(String shaSum) {
         String oneCommitUrlMock = urlMock + "/" + shaSum;
-        HttpEntity<?> requestEntity = new HttpEntity<Object>(new TemplateFactory().getHttpHeaders().setAuth(privateTokenMock).build());
+        HttpEntity<?> requestEntity = new HttpEntity<>(new TemplateFactory().getHttpHeaders().setAuth(privateTokenMock).build());
         ResponseEntity<Commit> response = new TemplateFactory().getRestTemplate().exchange(oneCommitUrlMock, HttpMethod.GET, requestEntity,
                 new ParameterizedTypeReference<Commit>() {
                 });
--- src/main/java/pl/hycom/jira/plugins/gitlab/integration/service/CommitService.java
+++ src/main/java/pl/hycom/jira/plugins/gitlab/integration/service/CommitService.java
@@ -1,4 +1,20 @@
 package pl.hycom.jira.plugins.gitlab.integration.service;
+/*
+ * <p>Copyright (c) 2016, Damian Deska, Kamil Rogowski
+ * Project:  gitlab-integration.</p>
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at</p>
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0</p>
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.</p>
+ */
 
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +31,7 @@ import java.util.List;
 @Log4j
 public class CommitService implements ICommitService {
 
-    //FIXME
+
     @Autowired
     private ICommitDao commitRepository;
 
