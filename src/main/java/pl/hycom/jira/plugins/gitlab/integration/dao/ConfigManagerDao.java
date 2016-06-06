1d2630c990f95a7f2c0872e40c4c56e684423c76
mkudl <sgt.dentons@gmail.com>
2016-06-06 17:21:04 +0200

    bugfix

--- src/main/java/pl/hycom/jira/plugins/gitlab/integration/dao/ConfigManagerDao.java
+++ src/main/java/pl/hycom/jira/plugins/gitlab/integration/dao/ConfigManagerDao.java
@@ -1,10 +1,14 @@
 package pl.hycom.jira.plugins.gitlab.integration.dao;
 
+import org.springframework.stereotype.Service;
+
 import java.sql.SQLException;
 
 /**
  * Created by vagrant on 5/17/16.
  */
+
+@Service
 public interface ConfigManagerDao
 {
     ConfigEntity getProjectConfig(int projectID) throws SQLException;
