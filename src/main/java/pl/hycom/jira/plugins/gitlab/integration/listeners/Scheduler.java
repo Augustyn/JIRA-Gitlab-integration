0f8bf01413dfc29ecb5ce130210a0d622207e89f
Tomasz <189675@edu.p.lodz.pl>
2016-06-10 18:47:05 +0200

    Dodanie zaleznosci Springowych

--- src/main/java/pl/hycom/jira/plugins/gitlab/integration/listeners/Scheduler.java
+++ src/main/java/pl/hycom/jira/plugins/gitlab/integration/listeners/Scheduler.java
@@ -1,13 +1,13 @@
 package pl.hycom.jira.plugins.gitlab.integration.listeners;
 
-import com.atlassian.sal.api.scheduling.PluginJob;
-import com.atlassian.sal.api.scheduling.PluginScheduler;
+
 import com.atlassian.sal.api.lifecycle.LifecycleAware;
+import com.atlassian.scheduler.SchedulerService;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import pl.hycom.jira.plugins.gitlab.integration.service.CommitService;
+
 import java.util.Date;
 import java.util.HashMap;
 
@@ -15,13 +15,14 @@ import java.util.HashMap;
 /**
  * Created by Tomek on 15/14/16.
  */
+
 @Log4j
 public class Scheduler implements IScheduler, LifecycleAware {
     @Getter
     private static final String KEY = Scheduler.class.getName() + "instance";
     private static final String JOB_NAME = "Schedules info about commits"; // nazwa zadania
     @Autowired
-    private PluginScheduler pluginScheduler;
+    private SchedulerService pluginScheduler;
     private long interval = 300000L;
     @Setter
     private Date lastRun = null;
@@ -30,23 +31,17 @@ public class Scheduler implements IScheduler, LifecycleAware {
         reschedule(interval);
     }
 
-    public void onStop(){
+    public void onStop() {
     }
 
-     public void reschedule(long interval) {
+    public void reschedule(long interval) {
         this.interval = interval;
 
-         HashMap params = new HashMap<String,Object>();
-         params.put(KEY, Scheduler.this);
+        HashMap params = new HashMap<String, Object>();
+        params.put(KEY, Scheduler.this);
 
-         pluginScheduler.scheduleJob(
-                JOB_NAME,             // unikalna nazwa zadania do wykonania
-                CommitService.class,     // klasa zadania do wykonania
-                params,
-                new Date(),                 // czas kiedy praca jest rozpoczynana
-                interval);                  // co jaki czas w milisekundach zadanie bedzie wykonywane
-        log.info(String.format("Informacje o comittach sa pobierane co %dms", interval));
+
+        //pluginScheduler.scheduleJob();
+
     }
-
-
 }
