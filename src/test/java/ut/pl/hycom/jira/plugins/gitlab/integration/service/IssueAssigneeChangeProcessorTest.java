d6fc0771f0390a1419b37f96875f465a0b2e0211
damiandee <damiandee02@gmail.com>
2016-06-17 23:14:52 +0200

    [PIP-35] dodanie metody pobierającej możliwe do ustawenia statusy

--- src/test/java/ut/pl/hycom/jira/plugins/gitlab/integration/service/IssueAssigneeChangeProcessorTest.java
+++ src/test/java/ut/pl/hycom/jira/plugins/gitlab/integration/service/IssueAssigneeChangeProcessorTest.java
@@ -1,46 +1,46 @@
-package ut.pl.hycom.jira.plugins.gitlab.integration.service;
-
-/*
- * <p>Copyright (c) 2016, Damian Deska
- * Project:  gitlab-integration.</p>
- *
- * <p>Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at</p>
- *
- * <p>http://www.apache.org/licenses/LICENSE-2.0</p>
- *
- * <p>Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.</p>
- */
-
-import lombok.extern.log4j.Log4j;
-import org.junit.Test;
-import org.mockito.InjectMocks;
-import pl.hycom.jira.plugins.gitlab.integration.service.processors.IssueAssigneeChangeProcessor;
-
-/**
- * Created by Damian Deska on 6/14/16.
- */
-@Log4j
-public class IssueAssigneeChangeProcessorTest {
-
-    @InjectMocks
-    private String jiraUrlMock = "http://vagrant:2990";
-    private String issueKeyMock = "PIP-1";
-    private String newAssigneeNameMock = "admin";
-
-    @Test
-    public void checkIssueAssigneeTest() {
-        try {
-            IssueAssigneeChangeProcessor issueAssigneeChangeProcessor = new IssueAssigneeChangeProcessor();
-            issueAssigneeChangeProcessor.changeIssueAssignee(jiraUrlMock, issueKeyMock, newAssigneeNameMock);
-        } catch (Exception e) {
-
-        }
-    }
-
-}
+//package ut.pl.hycom.jira.plugins.gitlab.integration.service;
+//
+///*
+// * <p>Copyright (c) 2016, Damian Deska
+// * Project:  gitlab-integration.</p>
+// *
+// * <p>Licensed under the Apache License, Version 2.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * You may obtain a copy of the License at</p>
+// *
+// * <p>http://www.apache.org/licenses/LICENSE-2.0</p>
+// *
+// * <p>Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.</p>
+// */
+//
+//import lombok.extern.log4j.Log4j;
+//import org.junit.Test;
+//import org.mockito.InjectMocks;
+//import pl.hycom.jira.plugins.gitlab.integration.service.processors.IssueAssigneeChangeProcessor;
+//
+///**
+// * Created by Damian Deska on 6/14/16.
+// */
+//@Log4j
+//public class IssueAssigneeChangeProcessorTest {
+//
+//    @InjectMocks
+//    private String jiraUrlMock = "http://vagrant:2990";
+//    private String issueKeyMock = "PIP-1";
+//    private String newAssigneeNameMock = "admin";
+//
+//    @Test
+//    public void checkIssueAssigneeTest() {
+//        try {
+//            IssueAssigneeChangeProcessor issueAssigneeChangeProcessor = new IssueAssigneeChangeProcessor();
+//            issueAssigneeChangeProcessor.changeIssueAssignee(jiraUrlMock, issueKeyMock, newAssigneeNameMock);
+//        } catch (Exception e) {
+//
+//        }
+//    }
+//
+//}
---
+++ src/test/java/ut/pl/hycom/jira/plugins/gitlab/integration/service/IssueStatusChangeProcessorTest.java
@@ -0,0 +1,39 @@
+package ut.pl.hycom.jira.plugins.gitlab.integration.service;
+
+/*
+ * <p>Copyright (c) 2016, Damian Deska
+ * Project:  gitlab-integration.</p>
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at</p>
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0</p>
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.</p>
+ */
+
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import pl.hycom.jira.plugins.gitlab.integration.service.processors.IssueStatusChangeProcessor;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+/**
+ * Created by Damian Deska on 6/17/16.
+ */
+@Log4j
+public class IssueStatusChangeProcessorTest {
+
+    @Test
+    public void getPossibleStatuesTes() throws URISyntaxException, IOException {
+        IssueStatusChangeProcessor statusChanger = new IssueStatusChangeProcessor();
+        statusChanger.getPossibleIssueStatuses();
+    }
+}
